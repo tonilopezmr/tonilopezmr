df1898a1f6cc4f726d2c36cda3a2a0c6508c8dd0
toni <tonilopezmr@gmail.com>
2025-07-06 23:06:20 -0700

    small improvement

--- new/src/pages/index.jsx
+++ new/src/pages/index.jsx
@@ -288,7 +288,7 @@ export default function Home({ articles }) {
             Product, Maker and Software engineer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Toni, a software designer and entrepreneur based in San Francisco 🌁, originally from Valencia ☀️. I'm a product builder with a passion for creating innovative solutions that cater to the needs of users and businesses alike. With a strong background in software engineering, I thrive on transforming ideas into reality through robust and user-friendly products.
+            I'm Toni, night hacker and entrepreneur by surprise based in San Francisco 🌁, originally from Valencia ☀️. I'm a product builder with a passion for creating awesome solutions from real problems, I only press keys for solving real problems.
             {/* I’m the founder and CTO of Cactus, where we develop
             technologies that empower regular people to explore space on their
             own terms. */}
